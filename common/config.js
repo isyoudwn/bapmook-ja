ab8583bde92eff13cdda7da18bd27ad642e93d8f
임채연 <dlacodus0407@gmail.com>
2025-06-13 08:44:38 +0900

    fix : resolve  env file path error

--- common/config.js
+++ common/config.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-configDotenv({ path: path.resolve(__dirname, '.env') });
+configDotenv({ path: path.resolve(__dirname, '../.env') });
 
 export const PORT = process.env.PORT;
 export const SLACK_TOKEN = process.env.SLACK_TOKEN;
--- common/scheduleHanlder.js
+++ common/scheduleHanlder.js
@@ -1,7 +1,15 @@
 import nodeCron from "node-cron";
 import { sendMessageToSlack } from "./slackHandler.js";
 
-export const startCron = () => nodeCron.schedule('0 10 * * 1-5', () => {
-    console.log('[10AM] 슬랙 전송 트리거 실행됨 (월~금)');
+export const startCron = () => nodeCron.schedule('30 9 * * 1-5', () => {
+    console.log('[9:30AM] 슬랙 전송 트리거 실행됨 (월~금)');
     sendMessageToSlack();
 });
+
+/**
+ * 테스트용 cron
+ */
+// export const startCron = () => nodeCron.schedule('0 * * * * *', () => {
+//     console.log('매분 실행되는 작업');
+//     sendMessageToSlack();
+// });
